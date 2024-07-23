674096260dff97c3c368b8595fef18acfc072259
RandomNumberGenerated91 <126611539+RandomNumberGenerated91@users.noreply.github.com>
2024-07-23 12:05:00 -0400

    added a basic menu
    the functions aren't implemented nor a way to select items from the menu

--- crates/arkham_skin_manager/src/main.rs
+++ crates/arkham_skin_manager/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
+
+    //I'll figure out the whole file structure stuff and reading the profile config file later.....
+   functionselection();
+}
+
+
+fn profileselection(){
+
+
     println!("Hello, world!");
+
+    println!("Create a new profile or select one ?");
+
+    println!("placeholder1");
+
+    println!("placeholder2");
+
+    println!("placeholder3");
+
+    println!("Create a new profile ! :D");
+
+
+
+}
+
+fn functionselection(){
+
+    println!("stuff goes here... \n");
+
+    println!("1. Add a new mod \n");
+    println!("2. Delete a mod \n");
+    println!("3. Change mod loadout \n");
+    println!("4. Save mod loadout \n");
+    println!("5. Back to profiles menu \n");
+    println!("6. Exit Program \n");
+
+    // a match statement will be used to select a menu item :3
+
+
 }
---
+++ crates/arkham_skin_manager/target/debug/arkham_skin_manager.d
@@ -0,0 +1,1 @@
+C:\Users\Anish\Documents\Rust\ projects\Arkham\ Game\ Skin\ Manager\crates\arkham_skin_manager\target\debug\arkham_skin_manager.exe: C:\Users\Anish\Documents\Rust\ projects\Arkham\ Game\ Skin\ Manager\crates\arkham_skin_manager\src\main.rs
